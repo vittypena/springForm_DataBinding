e4d87fafb86ec920cfb2f0ceda32ed4091289ee7
Victor Pena <victorpenawow@gmail.com>
2022-04-10 21:55:49 +0200

    Primer commit

---
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/services/PaisServiceImplement.java
@@ -0,0 +1,44 @@
+package com.bolsadeideas.springboot.form.app.services;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bolsadeideas.springboot.form.app.models.domain.Pais;
+
+@Service
+public class PaisServiceImplement implements PaisService {
+
+	private List<Pais> lista;
+	
+	public PaisServiceImplement() {
+		this.lista = Arrays.asList(
+				new Pais(1,"ES","España"), 
+				new Pais(2,"ME","Mexico"), 
+				new Pais(3,"CH","Chile"), 
+				new Pais(4,"AR","Argentina"), 
+				new Pais(5,"PE","Peru"), 
+				new Pais(6,"CO","Colombia"), 
+				new Pais(7,"VE","Venezuela")
+				);
+	}
+
+	@Override
+	public List<Pais> listar() {
+		return lista;
+	}
+
+	@Override
+	public Pais obtenerPorId(Integer id) {
+		Pais resultado = null;
+		for(Pais pais: this.lista) {
+			if(id == pais.getId()) {
+				resultado = pais;
+				break;
+			}
+		}
+		return resultado;
+	}
+
+}
